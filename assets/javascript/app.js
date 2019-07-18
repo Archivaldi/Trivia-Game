da1a975c1951be3f46e0a4be878f8bb1ec5c23ac
Artur Markov <artur.markov1860@gmail.com>
2019-07-18 13:18:07 -0700

    Second question functioning works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,13 +7,18 @@ var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unansweredQuestions = 0;
 
+
+
+function Option(str) {
+    this.text = str;
+}
+
 //reset timer for question
 function newTime() {
     questionTimer = 30;
     $("span").text(questionTimer + " Seconds");
 }
 
-//create brand new paragraphs for options on page
 function createOptions() {
     for (var i = 0; i < options.length; i++) {
         $("div").append("<button>");
@@ -22,10 +27,6 @@ function createOptions() {
     }
 }
 
-
-function Option(str) {
-    this.text = str;
-}
 
 
 function firstQuestion() {
@@ -39,14 +40,14 @@ function firstQuestion() {
         var option4 = new Option("Library of Congress");
         options.push(option1, option2, option3, option4);
     }
-        initOptions();
-        createOptions();
-        answer = options[2].text;
-        newTime();
+    initOptions();
+    createOptions();
+    answer = options[2].text;
+    newTime();
 
 }
 
-$("#start").on("click", function() {
+$("#start").on("click", function () {
     $("div").append("<h1>");
     $("h1").text("Totally Trivia Trivia!");
     $("div").append("<h3>");
@@ -57,36 +58,99 @@ $("#start").on("click", function() {
     $(this).remove();
     var timerForDisplay = setInterval(timeDown, 1000);
     firstQuestion();
-        timeDown();
+    timeDown();
 
-        $(".option").on("click", function () {
-            if ($(this).text() == answer) {
-                $("h2").text("Correct!");
-                correctAnswers += 1;
-                clearInterval(timerForDisplay);
-                $("button").remove();
-            } else {
-                $("h2").text("Nope!");
-                $("div").append("<p>");
-                $("p").text("Correct answer was " + answer);
-                incorrectAnswers =+ 1;
-                clearInterval(timerForDisplay);
-                $("button").remove();
-            }
+    $(".option").on("click", function () {
+        if ($(this).text() == answer) {
+            $("h2").text("Correct!");
+            correctAnswers += 1;
+            clearInterval(timerForDisplay);
+            $("button").remove();
+            options = [];
+            setTimeout(secondQuestion, 3 * 1000);
+        } else {
+            $("h2").text("Nope!");
+            $("div").append("<p>");
+            $("p").text("Correct answer was " + answer);
+            incorrectAnswers = + 1;
+            clearInterval(timerForDisplay);
+            $("button").remove();
+            options = [];
+            setTimeout(secondQuestion, 3 * 1000);
+        }
     })
 
     //set timer from 30 to 0
-function timeDown() {
-    if (questionTimer == 0) {
-        $("h2").text("Time is Out!");
-        $("div").append("<p>");
-        $("p").text("Correct answer was " + answer);
-        unansweredQuestions += 1;
-        clearInterval(timerForDisplay);
-        $("button").remove();
-    } else {
-        questionTimer -= 1;
-        $("span").text(questionTimer + " Seconds");
-    }
+    function timeDown() {
+        if (questionTimer == 0) {
+            $("h2").text("Time is Out!");
+            $("div").append("<p>");
+            $("p").text("Correct answer was " + answer);
+            unansweredQuestions += 1;
+            clearInterval(timerForDisplay);
+            $("button").remove();
+        } else {
+            questionTimer -= 1;
+            $("span").text(questionTimer + " Seconds");
+        }
     }
 })
+
+/////////////////////////////////////////////////////////Second Question//////////////////////////////////////////////////////////////
+
+function secondQuestion() {
+    question = "Whadfgsdgf?"
+    $("h2").text(question);
+    var timerForDisplay = setInterval(timeDown, 1000);
+
+    function timeDown() {
+        if (questionTimer == 0) {
+            $("h2").text("Time is Out!");
+            $("div").append("<p>");
+            $("p").text("Correct answer was " + answer);
+            unansweredQuestions += 1;
+            clearInterval(timerForDisplay);
+            $("button").remove();
+        } else {
+            questionTimer -= 1;
+            $("span").text(questionTimer + " Seconds");
+        }
+    }
+
+
+    function initOptions() {
+        var option1 = new Option("SF Pubdfgdfy");
+        var option2 = new Option("jdfgdfgy");
+        var option3 = new Option("Bodfgdfgrap");
+        var option4 = new Option("Librdfgdfgngress");
+        options.push(option1, option2, option3, option4);
+    }
+    initOptions();
+    createOptions();
+    answer = options[3].text;
+    newTime();
+
+    $(".option").on("click", function () {
+        if ($(this).text() == answer) {
+            $("h2").text("Correct!");
+            correctAnswers += 1;
+            clearInterval(timerForDisplay);
+            $("button").remove();
+            options = [];
+            setTimeout(secondQuestion, 3 * 1000);
+        } else {
+            $("h2").text("Nope!");
+            $("div").append("<p>");
+            $("p").text("Correct answer was " + answer);
+            incorrectAnswers = + 1;
+            clearInterval(timerForDisplay);
+            $("button").remove();
+            options = [];
+            setTimeout(secondQuestion, 3 * 1000);
+        }
+    })
+
+
+}
+
+/////////////////////////////////Third Question ///////////////////////////////////////////////
